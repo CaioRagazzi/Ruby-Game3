d605a33d55e925fc1f03232b5291d734ec3bf31d
Caio <ca.ragazzi@gmail.com>
2017-04-02 20:31:12 -0300

    new version

--- fogefoge.rb
+++ fogefoge.rb
@@ -6,6 +6,55 @@ def le_mapa(numero)
 	mapa = texto.split "\n"
 end
 
+def move_fantasmas(mapa)
+	caractere_do_fantasma = "F"
+	novo_mapa = copia_mapa mapa
+	mapa.each_with_index do |linha_atual, linha|
+		linha_atual.chars.each_with_index do |caractere_atual, coluna|
+			eh_fantasma = caractere_atual == caractere_do_fantasma
+			if eh_fantasma
+				move_fantasma mapa, novo_mapa, linha, coluna
+			end
+		end
+	end
+	novo_mapa
+end
+
+def soma_vetor(vetor1, vetor2)
+	[vetor1[0] + vetor2[0], vetor1[1] + vetor2[1]]
+end
+
+def posicoes_validas_a_partir_de(mapa, novo_mapa, posicao)
+	posicoes = []
+	movimentos = [[+1, 0], [0, +1],[-1, 0], [0, -1]]
+	movimentos.each do |movimento|
+		nova_posicao = soma_vetor(movimento, posicao)
+		if posicao_valida?(mapa, nova_posicao) && posicao_valida?(novo_mapa, nova_posicao)
+			posicoes << nova_posicao
+		end
+	end
+
+    posicoes
+end
+
+def copia_mapa(mapa)
+	novo_mapa = mapa.join("\n").tr("F", " ").split "\n"
+	novo_mapa
+end
+
+def move_fantasma(mapa, novo_mapa, linha,coluna)
+	posicoes = posicoes_validas_a_partir_de mapa,novo_mapa, [linha,coluna]
+	if posicoes.empty?
+		return
+	end
+
+	aleatoria = rand posicoes.size
+	posicao = posicoes[aleatoria]
+	mapa[linha][coluna] = " "
+	novo_mapa[posicao[0]][posicao[1]] = "F"
+	
+end
+
 def encontra_jogador(mapa)
 	caractere_do_heroi = "H"
 	mapa.each_with_index do |linha_atual,linha|
@@ -14,21 +63,20 @@ def encontra_jogador(mapa)
 			return [linha,coluna_do_heroi]
 		end
 	end
-	#nao achei!
+	nil
 end
 
 def calcula_nova_posicao(direcao,heroi)
 	heroi = heroi.dup
-	case direcao
-		when "W"
-			heroi[0] -= 1
-		when "S"
-			heroi[0] += 1
-		when "A"
-			heroi[1] -= 1
-		when "D"
-			heroi[1] += 1
-	end
+	movimentos = {
+		"W" => [-1, 0],
+		"S" => [+1, 0],
+		"A" => [0, -1],
+		"D" => [0, +1]
+	}
+	movimento = movimentos[direcao]
+	heroi[0] += movimento[0]
+	heroi[1] += movimento[1]
 	heroi
 end
 
@@ -38,17 +86,23 @@ def posicao_valida?(mapa, posicao)
 	estourou_linhas = posicao[0] < 0 || posicao[0] >= linhas
 	estourou_colunas = posicao[1] < 0 || posicao[1] >= colunas
 
-	if mapa[posicao[0]][posicao[1]] == "X"
+	if estourou_linhas || estourou_colunas
 		return false
 	end
-	if estourou_linhas || estourou_colunas
+
+	valor_atual = mapa[posicao[0]][posicao[1]]
+	if valor_atual == "X" || valor_atual == "F"
 		return false
 	end
 	true
 end
 
+def jogador_perdeu?(mapa)
+	perdeu = !encontra_jogador(mapa)
+end
+
 def joga(nome)
-	mapa = le_mapa 1
+	mapa = le_mapa 2
 
 	while true
 		desenha mapa
@@ -62,6 +116,13 @@ def joga(nome)
 
 		mapa[heroi[0]][heroi[1]] = " "
 		mapa[nova_posicao[0]][nova_posicao[1]] = "H"
+
+		mapa = move_fantasmas mapa
+
+		if jogador_perdeu?(mapa)
+			game_over
+			break
+		end
 	end
 end
 
--- ui.rb
+++ ui.rb
@@ -18,3 +18,9 @@ def pede_movimento
 	puts "Para onde deseja ir"
 	movimento = gets.strip
 end
+
+def game_over
+	puts "\n\n\n"
+	puts "Game over"
+
+end
